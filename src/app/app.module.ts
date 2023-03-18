e0433adfe5eda2d19f3a1248c7db5a272f036c28
Khaled Bribri <94048267+Khaled-Bribri@users.noreply.github.com>
2023-03-18 10:10:34 +0100

    add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FirstComponentComponent } from './components/first-component/first-component.component';
-import { SecondComponentComponent } from './components/second-component/second-component.component';
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponentComponent,
-    SecondComponentComponent,
   ],
   imports: [
     BrowserModule,
